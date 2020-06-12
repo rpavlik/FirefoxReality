7a74a943b2ba7e54fa09c60a97039da726acff75
Randall E. Barker <simstorm@mac.com>
2020-06-12 16:37:57 -0700

    wip

--- app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
+++ app/src/common/shared/org/mozilla/vrbrowser/VRBrowserActivity.java
@@ -143,6 +143,8 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
         }
     }
 
+    static final String LOGTAG = SystemUtils.createLogtag(VRBrowserActivity.class);
+
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
@@ -154,7 +156,6 @@ public class VRBrowserActivity extends PlatformActivity implements WidgetManager
     static final int SwipeDelay = 1000; // milliseconds
     static final long RESET_CRASH_COUNT_DELAY = 5000;
 
-    static final String LOGTAG = SystemUtils.createLogtag(VRBrowserActivity.class);
     HashMap<Integer, Widget> mWidgets;
     private int mWidgetHandleIndex = 1;
     AudioEngine mAudioEngine;
